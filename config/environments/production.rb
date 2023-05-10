73b45e191a228d4784c4052267e529275f937de4
abchung <anthony.b.chung@gmail.com>
2023-05-10 23:47:50 +1000

    env prod and dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "railwayapp-production-8fd2.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
